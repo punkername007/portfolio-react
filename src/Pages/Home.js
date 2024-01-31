72d447946a55176cb51c2e7acb52672163ff6075
Selene Roa <152019449+punkername007@users.noreply.github.com>
2024-01-31 02:24:01 +0100

    Added Hero

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,15 @@
 import React from "react";
+import "../Pages/styles/Home.css";
 
 export default function Home() {
-  return <div className="Home">Homepage</div>;
+  return (
+    <div className="Home">
+      <header className="container-fluid hero-container">
+        <div className="row align-items-center">
+          <h1 className="col-lg-6">Selene Roa</h1>
+          <h2 className="col-lg-6 text-lg-end">front-end developer</h2>
+        </div>
+      </header>
+    </div>
+  );
 }
