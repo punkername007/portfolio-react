baf8dbe1b5b15e431d08c043795a800be89d9253
Selene Roa <152019449+punkername007@users.noreply.github.com>
2024-01-31 02:05:54 +0100

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Navigation from "./Navigation";
+import Work from "./Pages/Work";
+import Contact from "./Pages/Contact";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Navigation />}>
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/work" element={<Work />} />
+      <Route path="/contact" element={<Contact />} />
+    </Route>
+  )
+);
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Navigation.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Navigation.css";
+import { NavLink, Outlet } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+
+export default function Navigation() {
+  return (
+    <div className="Navigation ">
+      <Navbar expand="lg" variant="dark" fixed="top" className="navbar">
+        <Navbar.Brand href="/" className="ms-4 navbar-brand">
+          &lt; <b>SR/</b>&gt;
+        </Navbar.Brand>
+        <Navbar.Toggle className="me-4" />
+        <Navbar.Collapse className="ms-4">
+          <Nav className="navbar-links ms-auto me-4">
+            <NavLink to="/" className="me-4">
+              Home
+            </NavLink>
+            <NavLink to="/about" className="me-4">
+              About
+            </NavLink>
+            <NavLink to="/work" className="me-4">
+              Work
+            </NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
